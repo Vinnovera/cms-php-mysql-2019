ab0b7d1fd5b6326ba10e4d5916d57746e59573c8
Jonas Stattin <jonas.i.stattin@gmail.com>
2019-04-18 06:47:06 +0200

    Base_site changes

--- exercises/base_site/base/contact.php
+++ exercises/base_site/base/contact.php
@@ -2,9 +2,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="style.css">
-  <link rel="shortcut icon" href="https://jesperorb.github.io/cat-deploy/favicon.png" type="image/x-icon">
   <title> Contact </title>
 </head>
 <body>
