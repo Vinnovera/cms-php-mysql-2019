d68972662660ca6c3748a7e251cf6ecd77df0e78
Jonas Stattin <jonas.i.stattin@gmail.com>
2019-05-13 13:26:36 +0200

    Minor cleanup

--- examples/slim/slim-pdo-boilerplate/public/index.php
+++ examples/slim/slim-pdo-boilerplate/public/index.php
@@ -28,4 +28,3 @@
 
   // Run app
   $app->run();
-?>
--- examples/slim/slim-pdo-boilerplate/src/container.php
+++ examples/slim/slim-pdo-boilerplate/src/container.php
@@ -1,8 +1,6 @@
 <?php
 
-use Slim\App;
-
-return function (App $app) {
+return function ($app) {
     $container = $app->getContainer();
 
     // database
