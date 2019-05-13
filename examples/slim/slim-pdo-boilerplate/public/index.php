e0f7073791067f7e0db7c0f66893b54da21d7b0a
Jonas Stattin <jonas.i.stattin@gmail.com>
2019-05-13 12:52:33 +0200

    Initial commit of Slim boilerplate

---
+++ examples/slim/slim-pdo-boilerplate/public/index.php
@@ -0,0 +1,31 @@
+<?php
+  // Load everything needed
+  require __DIR__ . '/../vendor/autoload.php';
+
+  // Start a session here
+  session_start();
+
+  // Get settings and instantiate the app
+  $settings = require __DIR__ . '/../src/settings.php';
+  $app = new \Slim\App($settings);
+
+  // Register our dependencies through our container
+  $dependencies = require __DIR__ . '/../src/container.php';
+  $dependencies($app);
+
+  // Start adding routes
+  $app->get('/user/{id}', function ($request, $response, $args) {
+    $userID = $args['id'];
+
+    $statement = $this->db->prepare("SELECT * FROM users WHERE userID = :userID");
+    $statement->execute([
+      ':userID' => $userID
+    ]);
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $response->withJson($user);
+  });
+
+  // Run app
+  $app->run();
+?>
---
+++ examples/slim/slim-pdo-boilerplate/src/container.php
@@ -0,0 +1,21 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    // database
+    $container['db'] = function ($c) {
+      $db = $c['settings']['db'];
+      $pdo = new PDO(
+        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'] . ";charset=utf8",
+        $db['user'],
+        $db['pass']
+      );
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+      // We must always return what we want to inject
+      return $pdo;
+    };
+};
